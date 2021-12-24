5d3bfb14e51e5c80472468385baf19906d923605
Soham Sarkar <soham@greedygame.com>
2021-12-24 12:47:51 +0530

    add photo route

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -4,22 +4,41 @@ import { Photo } from "../entity/photo.entity";
 
 export const getAllPhotos = async (_req: Request, res: Response) => {
   try {
-    //if no photos in db then send random urls
-
     const photos = await Photo.find();
 
-    if (!photos.length) {
-      return res.json(items);
+    switch (true) {
+      case !photos.length:
+        return res.json(items);
+      case photos.length < 10:
+        return res.json([...photos, ...items]);
+      default:
+        return res.json(photos);
     }
-
-    return res.json([...photos, ...items]);
   } catch (err) {
     res.status(400).json({ err });
   }
 };
 
-export const addPhoto = (req: Request, res: Response) => {
-  const body = req.body;
-  console.log(body);
-  res.json({ status: "success" });
+export const addPhoto = async (req: Request, res: Response) => {
+  try {
+    const { label, url } = req.body;
+    console.log(label, url);
+
+    if (!label) {
+      res.status(400).json({ msg: "no label provided" });
+    }
+    if (!url) {
+      res.status(400).json({ msg: "no url provided" });
+    }
+
+    const photo = new Photo();
+
+    photo.label = label;
+    photo.url = url;
+
+    await photo.save();
+    res.json({ status: "success", data: photo });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 };
